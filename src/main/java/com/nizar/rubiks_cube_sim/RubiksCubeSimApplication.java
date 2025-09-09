90a3fa7dc3a6a28cf2982dd7ef9604188d3691af
Nizar <nizar.tourabi13@gmail.com>
2025-09-09 14:19:53 +0100

    Initial project setup and commit.

---
+++ src/main/java/com/nizar/rubiks_cube_sim/RubiksCubeSimApplication.java
@@ -0,0 +1,13 @@
+package com.nizar.rubiks_cube_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiksCubeSimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubiksCubeSimApplication.class, args);
+    }
+
+}
