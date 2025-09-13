ba6a2594d1292fedc9a809b692c957d4fdc66071
Nizar <nizar.tourabi13@gmail.com>
2025-09-13 12:50:47 +0100

    Added semi colon and minimal Javadocs to Colour enum.

--- src/main/java/com/nizar/rubiks_cube_sim/model/Colour.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Colour.java
@@ -1,10 +1,14 @@
 package com.nizar.rubiks_cube_sim.model;
 
+/**
+ * ENUM: Colour
+ * All possible types of tile colour on a Rubik's cube.
+ */
 public enum Colour {
     RED,
     ORANGE,
     YELLOW,
     GREEN,
     BLUE,
-    WHITE
+    WHITE;
 }
