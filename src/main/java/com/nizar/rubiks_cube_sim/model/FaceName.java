1f86371066ce1f40288c6edc1ef05dab4b2a611c
Nizar <nizar.tourabi13@gmail.com>
2025-09-13 15:50:47 +0100

    New global mapping function which allows you to get the bordering edges of each face on the cube. Necessary for any cube moves/interactions.

--- src/main/java/com/nizar/rubiks_cube_sim/model/FaceName.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/FaceName.java
@@ -17,4 +17,55 @@ public enum FaceName {
     public String getNotation() {
         return notation;
     }
+
+    /**
+     * Cube pattern:
+     *   U
+     * L F R B
+     *   D
+     * Each face numbered 1-9, left to right and top to bottom.
+     *
+     * @return all the edges that border a particular face.
+     */
+    public FaceEdge[] getBorderingEdges() {
+        return switch(this) {
+            case FRONT -> new FaceEdge[] {
+                    new FaceEdge(UP, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(DOWN, EdgeType.TOP_EDGE),
+                    new FaceEdge(LEFT, EdgeType.RIGHT_EDGE),
+                    new FaceEdge(RIGHT, EdgeType.LEFT_EDGE)
+            };
+            case BACK -> new FaceEdge[] {
+                    new FaceEdge(UP, EdgeType.TOP_EDGE),
+                    new FaceEdge(DOWN, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(LEFT, EdgeType.LEFT_EDGE),
+                    new FaceEdge(RIGHT, EdgeType.RIGHT_EDGE)
+            };
+            case UP -> new FaceEdge[] {
+                    new FaceEdge(LEFT, EdgeType.TOP_EDGE),
+                    new FaceEdge(RIGHT, EdgeType.TOP_EDGE),
+                    new FaceEdge(FRONT, EdgeType.TOP_EDGE),
+                    new FaceEdge(BACK, EdgeType.TOP_EDGE)
+            };
+            case DOWN -> new FaceEdge[] {
+                    new FaceEdge(LEFT, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(RIGHT, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(FRONT, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(BACK, EdgeType.BOTTOM_EDGE)
+            };
+            case LEFT -> new FaceEdge[] {
+                    new FaceEdge(UP, EdgeType.LEFT_EDGE),
+                    new FaceEdge(DOWN, EdgeType.LEFT_EDGE),
+                    new FaceEdge(FRONT, EdgeType.LEFT_EDGE),
+                    new FaceEdge(BACK, EdgeType.RIGHT_EDGE)
+            };
+            case RIGHT -> new FaceEdge[] {
+                    new FaceEdge(UP, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(DOWN, EdgeType.TOP_EDGE),
+                    new FaceEdge(FRONT, EdgeType.RIGHT_EDGE),
+                    new FaceEdge(BACK, EdgeType.LEFT_EDGE)
+            };
+            default -> throw new IllegalStateException("Unexpected value: " + this);
+        };
+    }
 }
