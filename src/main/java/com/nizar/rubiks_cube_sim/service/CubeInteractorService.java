f826d7ed55b2bd20bf86d3c79b75809065cf1744
Nizar <nizar.tourabi13@gmail.com>
2025-09-13 16:03:26 +0100

    Added parameters and javadocs to the interface for interacting with the cube.

--- src/main/java/com/nizar/rubiks_cube_sim/service/CubeInteractorService.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/CubeInteractorService.java
@@ -1,9 +1,26 @@
 package com.nizar.rubiks_cube_sim.service;
 
+import com.nizar.rubiks_cube_sim.model.Cube;
 import com.nizar.rubiks_cube_sim.model.Face;
+import com.nizar.rubiks_cube_sim.model.Turn;
 
 public interface CubeInteractorService {
-    public void rotateFace();
-    public void rotateEdgesBorderingFace();
-    public void makeMove();
+    /**
+     * Takes a face and repaints it to simulate a rotation.
+     * @param face
+     */
+    public void rotateFace(Face face, Turn turn);
+
+    /**
+     * Each face has four bordering faces/edges. This will rotate the edges directly bordering one specific face.
+     * @param cube
+     * @param face
+     * @param turn
+     */
+    public void rotateEdgesBorderingFace(Cube cube, Face face, Turn turn);
+
+    /**
+     * This combines rotateFace() and rotateEdgesBorderingFace.
+     */
+    public void makeMove(Cube cube, Face face, Turn turn);
 }
