d4aed2927937abbf9b57ff80f95e41cf80900f0a
Nizar <nizar.tourabi13@gmail.com>
2025-09-14 17:49:48 +0100

    Changed Face to use a 1D array instead of 2D, to keep it consistent with other existing logic.

--- src/main/java/com/nizar/rubiks_cube_sim/model/Face.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Face.java
@@ -2,31 +2,31 @@ package com.nizar.rubiks_cube_sim.model;
 
 /**
  * MODEL CLASS: Face
- * Represents a face on a Rubik's Cube. Features a 2D array to represent 9 tiles.
+ * Represents a face on a Rubik's Cube. Features an array to represent 9 tiles.
  * Each tile is represented by a specific colour.
- * Each subarray is an entire row. Each subarray element represents a particular column in that row.
+ * Index 0 represents the top left. Next indices are obtained moving left to right, then  top to bottom.
  */
 public class Face {
-    private Colour[][] tiles;
+    private Colour[] tiles;
 
-    public Face(Colour[][] tiles) {
+    public Face(Colour[] tiles) {
         this.tiles = tiles;
     }
 
-    public Colour[][] getTiles() {
+    public Colour[] getTiles() {
         return tiles;
     }
 
-    public void setTiles(Colour[][] tiles) {
+    public void setTiles(Colour[] tiles) {
         this.tiles = tiles;
     }
 
-    public Colour getTile(int row, int column) {
-        return tiles[row][column];
+    public Colour getTile(int index) {
+        return tiles[index];
     }
 
-    public void setTile(int row, int column, Colour tile) {
-        tiles[row][column] = tile;
+    public void setTile(int index, Colour tile) {
+        tiles[index] = tile;
     }
 
 }
