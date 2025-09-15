4cdacdae9c724c94c1c0178ab223d80749c32c66
Nizar <nizar.tourabi13@gmail.com>
2025-09-15 13:33:30 +0100

    Modified borderingEdges method to get the bordering edges in a particular order (in-order = clockwise quarter).

--- src/main/java/com/nizar/rubiks_cube_sim/model/FaceName.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/FaceName.java
@@ -19,51 +19,52 @@ public enum FaceName {
     }
 
     /**
-     * Cube pattern:
+     * Cube net:
      *   U
      * L F R B
      *   D
      * Each face numbered 1-9, left to right and top to bottom.
-     *
+     * Each array of FaceEdges is deliberately pre-ordered for easy swapping / rotations. In-order traversal = Right Quarter Turn.
      * @return all the edges that border a particular face.
      */
     public FaceEdge[] getBorderingEdges() {
         return switch(this) {
             case FRONT -> new FaceEdge[] {
                     new FaceEdge(UP, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(RIGHT, EdgeType.LEFT_EDGE),
                     new FaceEdge(DOWN, EdgeType.TOP_EDGE),
-                    new FaceEdge(LEFT, EdgeType.RIGHT_EDGE),
-                    new FaceEdge(RIGHT, EdgeType.LEFT_EDGE)
+                    new FaceEdge(LEFT, EdgeType.RIGHT_EDGE)
             };
             case BACK -> new FaceEdge[] {
                     new FaceEdge(UP, EdgeType.TOP_EDGE),
-                    new FaceEdge(DOWN, EdgeType.BOTTOM_EDGE),
                     new FaceEdge(LEFT, EdgeType.LEFT_EDGE),
+                    new FaceEdge(DOWN, EdgeType.BOTTOM_EDGE),
                     new FaceEdge(RIGHT, EdgeType.RIGHT_EDGE)
             };
             case UP -> new FaceEdge[] {
-                    new FaceEdge(LEFT, EdgeType.TOP_EDGE),
+                    new FaceEdge(BACK, EdgeType.TOP_EDGE),
                     new FaceEdge(RIGHT, EdgeType.TOP_EDGE),
                     new FaceEdge(FRONT, EdgeType.TOP_EDGE),
-                    new FaceEdge(BACK, EdgeType.TOP_EDGE)
+                    new FaceEdge(LEFT, EdgeType.TOP_EDGE)
             };
             case DOWN -> new FaceEdge[] {
-                    new FaceEdge(LEFT, EdgeType.BOTTOM_EDGE),
-                    new FaceEdge(RIGHT, EdgeType.BOTTOM_EDGE),
                     new FaceEdge(FRONT, EdgeType.BOTTOM_EDGE),
-                    new FaceEdge(BACK, EdgeType.BOTTOM_EDGE)
+                    new FaceEdge(RIGHT, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(BACK, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(LEFT, EdgeType.BOTTOM_EDGE)
             };
             case LEFT -> new FaceEdge[] {
                     new FaceEdge(UP, EdgeType.LEFT_EDGE),
-                    new FaceEdge(DOWN, EdgeType.LEFT_EDGE),
                     new FaceEdge(FRONT, EdgeType.LEFT_EDGE),
+                    new FaceEdge(DOWN, EdgeType.LEFT_EDGE),
                     new FaceEdge(BACK, EdgeType.RIGHT_EDGE)
             };
             case RIGHT -> new FaceEdge[] {
                     new FaceEdge(UP, EdgeType.BOTTOM_EDGE),
+                    new FaceEdge(BACK, EdgeType.LEFT_EDGE),
                     new FaceEdge(DOWN, EdgeType.TOP_EDGE),
-                    new FaceEdge(FRONT, EdgeType.RIGHT_EDGE),
-                    new FaceEdge(BACK, EdgeType.LEFT_EDGE)
+                    new FaceEdge(FRONT, EdgeType.RIGHT_EDGE)
+
             };
             default -> throw new IllegalStateException("Unexpected value: " + this);
         };
