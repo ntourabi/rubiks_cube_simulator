1796f1ef4abb08b1b9c4396b8998597a12872c7a
Nizar <nizar.tourabi13@gmail.com>
2025-09-15 13:44:30 +0100

    Modified Face object to be slightly more defensive, ensuring 9 tiles in all circumstances per face. Also added method for creating a pre-filled face with a given colour.

--- src/main/java/com/nizar/rubiks_cube_sim/model/Face.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Face.java
@@ -7,10 +7,17 @@ package com.nizar.rubiks_cube_sim.model;
  * Index 0 represents the top left. Next indices are obtained moving left to right, then  top to bottom.
  */
 public class Face {
+    private static final int FACE_SIZE = 9;
+
     private Colour[] tiles;
 
     public Face(Colour[] tiles) {
-        this.tiles = tiles;
+        if (tiles == null) {
+            throw new IllegalArgumentException("Face object cannot have 'null' tiles.");
+        } else if (tiles.length == FACE_SIZE) {
+            throw new IllegalArgumentException("Face object must have exactly 9 tiles.");
+        }
+        this.tiles = tiles.clone();
     }
 
     public Colour[] getTiles() {
@@ -29,4 +36,7 @@ public class Face {
         tiles[index] = tile;
     }
 
+    public static Face createSolvedFace(Colour colour) {
+        return new Face(new Colour[] {colour, colour, colour, colour, colour, colour, colour, colour, colour});
+    }
 }
