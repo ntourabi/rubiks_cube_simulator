5517d3c880d8bde80a1a566dfcfbec4b67f91f1a
Nizar <nizar.tourabi13@gmail.com>
2025-09-15 13:59:41 +0100

    Modified Cube object to be slightly more defensive, preventing creation of cubes with null faces. Also added convenient static method for creating a solved cube.

--- src/main/java/com/nizar/rubiks_cube_sim/model/Cube.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Cube.java
@@ -1,12 +1,28 @@
 package com.nizar.rubiks_cube_sim.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cube {
     private Map<FaceName, Face> faces;
 
     public Cube(Map<FaceName, Face> faces) {
-        this.faces = faces;
+        if (faces == null) {
+            throw new IllegalArgumentException("You can't create a Cube with 'null' faces.");
+        }
+        this.faces = new HashMap<>(faces);
+    }
+
+    //Follows traditional cube convention.
+    public static Cube createSolvedCube() {
+        Map<FaceName, Face> solvedFaces = new HashMap<FaceName, Face>();
+        solvedFaces.put(FaceName.FRONT, Face.createSolvedFace((Colour.GREEN)));
+        solvedFaces.put(FaceName.BACK, Face.createSolvedFace((Colour.BLUE)));
+        solvedFaces.put(FaceName.RIGHT, Face.createSolvedFace((Colour.RED)));
+        solvedFaces.put(FaceName.LEFT, Face.createSolvedFace((Colour.ORANGE)));
+        solvedFaces.put(FaceName.UP, Face.createSolvedFace((Colour.WHITE)));
+        solvedFaces.put(FaceName.DOWN, Face.createSolvedFace((Colour.YELLOW)));
+        return new Cube(solvedFaces);
     }
 
     public Map<FaceName, Face> getFaces() {
@@ -25,3 +41,4 @@ public class Cube {
         faces.put(faceName, face);
     }
 }
+
