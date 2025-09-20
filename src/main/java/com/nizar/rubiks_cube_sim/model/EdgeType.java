0658e83778d4107e8077f3a25f0eb5d9e6879c42
Nizar <nizar.tourabi13@gmail.com>
2025-09-20 10:44:10 +0100

    Fixed mapping bug related to GitHub Issue #3. EdgeType.java is written as if the cube were indexed 1-9, as opposed to 0-8. Tested on top quarter clockwise rotations - functional.

--- src/main/java/com/nizar/rubiks_cube_sim/model/EdgeType.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/EdgeType.java
@@ -1,10 +1,10 @@
 package com.nizar.rubiks_cube_sim.model;
 
 public enum EdgeType {
-    TOP_EDGE(new int[]{1, 2, 3}),
-    BOTTOM_EDGE(new int[]{7, 8, 9}),
-    LEFT_EDGE(new int[]{1, 4, 7}),
-    RIGHT_EDGE(new int[]{3, 6, 9});
+    TOP_EDGE(new int[]{0, 1, 2}),
+    BOTTOM_EDGE(new int[]{6, 7, 8}),
+    LEFT_EDGE(new int[]{0, 3, 6}),
+    RIGHT_EDGE(new int[]{2, 5, 8});
 
     private int[] indices;
 
