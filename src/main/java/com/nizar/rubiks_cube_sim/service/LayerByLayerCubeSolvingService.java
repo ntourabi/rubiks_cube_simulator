1ef4b1c5fc87bcd7fb35d7431b55720834e053aa
Nizar <nizar.tourabi13@gmail.com>
2025-09-21 14:52:23 +0100

    Changed LayerByLayerCubeSolvingService interface to have a singular solving method which will utilize the recently created SolvingStage implementations, instead of having them as separate methods.

--- src/main/java/com/nizar/rubiks_cube_sim/service/LayerByLayerCubeSolvingService.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/LayerByLayerCubeSolvingService.java
@@ -3,24 +3,17 @@ package com.nizar.rubiks_cube_sim.service;
 import com.nizar.rubiks_cube_sim.model.Cube;
 
 public interface LayerByLayerCubeSolvingService {
-    //Stage 1
-    public String solveWhiteCross(Cube cube);
-    //Stage 2
-    public String solveWhiteCorners(Cube cube);
-    //Stage 3
-    public String solveMiddleLayerEdges(Cube cube);
-    //Stage 4
-    public String solveYellowCross(Cube cube);
-    //Stage 5
-    public String orientYellowCorners(Cube cube);
-    //Stage 6
-    public String permuteYellowCorners(Cube cube);
-    //Stage 7
-    public String permuteYellowEdges(Cube cube);
-
-    //Run through all stages.
+    /**
+     * Run through all seven stages of the layer by layer solving method.
+     * Stage 1 - solveWhiteCross
+     * Stage 2 - solveWhiteCorners
+     * Stage 3 - solveMiddleLayerEdges
+     * Stage 4 - solveYellowCross
+     * Stage 5 - orientYellowCorners
+     * Stage 6 - permuteYellowCorners
+     * Stage 7 - permuteYellowEdges
+     * @param cube
+     * @return
+     */
     public String solveCube(Cube cube);
-
-    //Apply a particular algorithm to the cube.
-    public String applyAlgorithm(Cube cube);
 }
