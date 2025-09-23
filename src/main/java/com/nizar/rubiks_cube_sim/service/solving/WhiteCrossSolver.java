a3c6d9a294c5703a3820d543fd31b6ad16ebd9ac
Nizar <nizar.tourabi13@gmail.com>
2025-09-23 15:10:43 +0100

    Created method for finding unsolved white edge pieces on a Rubik's cube. Added method call to relevant solver loop.

--- src/main/java/com/nizar/rubiks_cube_sim/service/solving/WhiteCrossSolver.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/solving/WhiteCrossSolver.java
@@ -1,10 +1,8 @@
 package com.nizar.rubiks_cube_sim.service.solving;
 
-import com.nizar.rubiks_cube_sim.model.Colour;
-import com.nizar.rubiks_cube_sim.model.Cube;
-import com.nizar.rubiks_cube_sim.model.Face;
-import com.nizar.rubiks_cube_sim.model.FaceName;
+import com.nizar.rubiks_cube_sim.model.*;
 
+import java.util.List;
 import java.util.Map;
 
 public class WhiteCrossSolver implements SolvingStage {
@@ -13,8 +11,9 @@ public class WhiteCrossSolver implements SolvingStage {
         Face whiteFace = getWhiteFace(cube);
         String sequence = "";
 
+
         while (!whiteCrossComplete(whiteFace)) {
-            break;
+            TileLocation unsolvedEdge = findUnsolvedWhiteEdgeTile(cube, whiteFace);
         }
 
         return sequence;
@@ -35,5 +34,32 @@ public class WhiteCrossSolver implements SolvingStage {
         return false;
     }
 
-    private void findUnsolvedWhiteEdge(Cube cube) {}
+    /**
+     * An unsolved white edge piece is:
+     *  - Not already on the white face, so its on one of the five other faces.
+     *  - Will be in index 1, 3, 5, or 7.
+     * We can put an unsolved white edge into place by:
+     *  - Identifying any target tile to solve.
+     *  - Selecting any unsolved white edge piece.
+     *  - Finding the sequence of moves necessary to move it into place.
+     *
+     * @param cube - The cube we are currently trying to solve.
+     * @param whiteFace - A reference to the face with a white middle tile.
+     * @return TileLocation - The name of the face, and the tile index (0-8).
+     */
+    private TileLocation findUnsolvedWhiteEdgeTile(Cube cube, Face whiteFace) {
+        Map<FaceName, Face> faces = cube.getFaces();
+        for (FaceName face : faces.keySet()) {
+            if (faces.get(face) == whiteFace) continue;
+            if (faces.get(face).getTile(1) == Colour.WHITE) return new TileLocation(face, 1);
+            if (faces.get(face).getTile(3) == Colour.WHITE) return new TileLocation(face, 3);
+            if (faces.get(face).getTile(5) == Colour.WHITE) return new TileLocation(face, 5);
+            if (faces.get(face).getTile(7) == Colour.WHITE) return new TileLocation(face, 7);
+        }
+        throw new RuntimeException("Couldn't find unsolved white edge on non-white faces.");
+    }
+
+    private String makeMove(Cube cube, TileLocation currentLocation, TileLocation targetLocation) {
+        return "";
+    }
 }
