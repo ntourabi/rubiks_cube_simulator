444fd40ab487b746032923419bdeca3cedef1cd8
Nizar <nizar.tourabi13@gmail.com>
2025-09-24 21:58:51 +0100

    Minor Javadocs improvements for Algorithm.java

--- src/main/java/com/nizar/rubiks_cube_sim/model/Algorithm.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Algorithm.java
@@ -2,6 +2,7 @@ package com.nizar.rubiks_cube_sim.model;
 
 /**
  * This enum is used to capture common algorithms used in various solving stages.
+ * Each algorithm is represented as a string describing a sequence of moves, written in standard cubing notation.
  */
 public enum Algorithm {
 }
