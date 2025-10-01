cc6e8c2a2c9ee28b58f190aec01103e700b542ab
Nizar <nizar.tourabi13@gmail.com>
2025-10-01 15:27:35 +0100

    Javadocs enhancement for CubeInteractorService.java

--- src/main/java/com/nizar/rubiks_cube_sim/service/CubeInteractorService.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/CubeInteractorService.java
@@ -1,31 +1,39 @@
 package com.nizar.rubiks_cube_sim.service;
 
-import com.nizar.rubiks_cube_sim.model.Cube;
-import com.nizar.rubiks_cube_sim.model.Face;
-import com.nizar.rubiks_cube_sim.model.FaceName;
-import com.nizar.rubiks_cube_sim.model.Turn;
+import com.nizar.rubiks_cube_sim.model.*;
 
 public interface CubeInteractorService {
     /**
      * Takes a face and repaints it to simulate a rotation.
-     * @param face
-     * @param turn
+     *
+     * @param face - The target face.
+     * @param turn - The type of move.
      */
     public void rotateFace(Face face, Turn turn);
 
     /**
      * Each face has four bordering faces/edges. This will rotate the edges directly bordering one specific face.
-     * @param cube
-     * @param face
-     * @param turn
+     *
+     * @param cube - The cube we are transforming.
+     * @param faceName - The target face.
+     * @param turn - The type of move.
      */
-    public void rotateEdgesBorderingFace(Cube cube, FaceName face, Turn turn);
+    public void rotateEdgesBorderingFace(Cube cube, FaceName faceName, Turn turn);
 
     /**
      * This combines rotateFace() and rotateEdgesBorderingFace.
-     * @param cube
-     * @param face
-     * @param turn
+     *
+     * @param cube - The cube we are transforming.
+     * @param faceName - The target face.
+     * @param turn - The type of move.
      */
-    public void makeMove(Cube cube, FaceName face, Turn turn);
+    public void makeMove(Cube cube, FaceName faceName, Turn turn);
+
+    /**
+     * Apply a common predefined sequence of moves (an algorithm) to the cube.
+     *
+     * @param cube - the cube to apply the algorithm to
+     * @param algorithm - the sequence of moves
+     */
+    public void applyAlgorithm(Cube cube, Algorithm algorithm);
 }
