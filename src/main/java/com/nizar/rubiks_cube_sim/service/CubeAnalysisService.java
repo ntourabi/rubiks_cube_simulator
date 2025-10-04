67c919fbbe3055039d2e76580b99267907fed99f
Nizar <nizar.tourabi13@gmail.com>
2025-10-04 20:27:20 +0100

    CubeAnalysisService.java should be an interface, not a class. Corrected.

--- src/main/java/com/nizar/rubiks_cube_sim/service/CubeAnalysisService.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/CubeAnalysisService.java
@@ -3,6 +3,6 @@ package com.nizar.rubiks_cube_sim.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CubeAnalysisService {
+public interface CubeAnalysisService {
 
 }
