5567acfe3e12d0cc6f6e0d928863d857d9e23556
Nizar <nizar.tourabi13@gmail.com>
2025-10-18 13:44:57 +0100

    Moved CubeAnalysisServiceImpl.java to correct package.

--- src/main/java/com/nizar/rubiks_cube_sim/service/analysis/CubeAnalysisServiceImpl.java
+++ src/main/java/com/nizar/rubiks_cube_sim/service/analysis/CubeAnalysisServiceImpl.java
@@ -1,4 +1,4 @@
-package com.nizar.rubiks_cube_sim.service;
+package com.nizar.rubiks_cube_sim.service.analysis;
 
 import com.nizar.rubiks_cube_sim.model.Cube;
 import com.nizar.rubiks_cube_sim.model.Face;
