695cbba59200350fb03254ecf9ea8a390896802f
Nizar Tourabi <nizar.tourabi13@gmail.com>
2025-11-26 12:00:33 +0000

    Comment

--- src/main/java/com/nizar/rubiks_cube_sim/model/Cubie.java
+++ src/main/java/com/nizar/rubiks_cube_sim/model/Cubie.java
@@ -1,5 +1,11 @@
 package com.nizar.rubiks_cube_sim.model;
 
+/**
+ * Three types of Cubie:
+ * - Corner Cubie (3 stickers)
+ * - Edge Cubie (2 stickers)
+ * - Centre (1 sticker, unmoving, possibly doesn't need representation)
+ */
 class Cubie {
 
 }
